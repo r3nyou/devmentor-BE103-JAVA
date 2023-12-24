a7662cb573001dcd9103974ce9025b195f2da0f0
Jian Marcus <renyou815@gmail.com>
2023-12-24 15:55:53 +0800

    first commit

---
+++ src/test/java/com/devmentor/restfulapidemo/RestfulApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmentor.restfulapidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
